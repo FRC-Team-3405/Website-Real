a60d9e8a522841338f1d60b8cea2b2f94bfa772f
sammer2605 <samuel.w139@stu.nebo.edu>
2022-10-06 16:22:15 -0600

    Add Countdown clock, caps logo, etc.

---
+++ javascript/countdown.js
@@ -0,0 +1,31 @@
+(function () {
+    const second = 1000,
+          minute = second * 60,
+          hour = minute * 60,
+          day = hour * 24;
+  
+        //   CHANGE THE DATE BELOW
+    kickoff = "01/07/2023 10:30 AM";
+    // CHANGE THE DATE ABOVE
+    
+    const countDown = new Date(kickoff).getTime(),
+        x = setInterval(function() {    
+  
+          const now = new Date().getTime(),
+                distance = countDown - now;
+  
+          document.getElementById("days").innerText = Math.floor(distance / (day)),
+            document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+            document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+  
+          //do something later when date is reached
+          if (distance < 0) {
+            document.getElementById("headline").innerText = "It's Kickoff Day!";
+            document.getElementById("countdown").style.display = "none";
+            document.getElementById("content").style.display = "block";
+            clearInterval(x);
+          }
+          //seconds
+        }, 0)
+    }());
